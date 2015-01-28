c3ae6801ca279403fdeb0f71a80f6190a27cab84
Adam Barrowman <adambarrowman@yahoo.ca>
2015-01-27 20:38:40 -0500

    Added a build script to the project.

--- src/stenography/StenographyDemo.java
+++ src/stenography/StenographyDemo.java
@@ -2,11 +2,8 @@ package stenography;
 
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,7 +12,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
@@ -23,20 +19,17 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.swing.filechooser.FileFilter;
 
 import core.CryptoUtils;
-import core.PairityBitType;
 import des.TripleDES;
 
 public class StenographyDemo implements ActionListener {
 
   private byte[] source;
   private BufferedImage mask;
-  private String state;
+  private String fileChooserState;
 
   private File stenFile;
 
@@ -99,7 +92,7 @@ public class StenographyDemo implements ActionListener {
   };
 
   public StenographyDemo() {
-    state = "";
+    fileChooserState = "";
 
     frame = new JFrame("Stenography Demo");
     passwordInput = new JPasswordField("abc");
@@ -147,7 +140,7 @@ public class StenographyDemo implements ActionListener {
       problems = "Select a source.";
     } else if (mask == null) {
       problems = "Select a mask.";
-    } else if (!PNGStenographer.canImageHoldData(source.length, mask.getWidth(), mask.getHeight())) {  
+    } else if (!PNGStenographer.canImageHoldData(source.length, mask.getWidth(), mask.getHeight())) {
       problems = "Select a larger mask or a smaller source.";
     }
 
@@ -164,19 +157,19 @@ public class StenographyDemo implements ActionListener {
   @Override
   public void actionPerformed(ActionEvent event) {
     if (event.getSource() == openButton) {
-      state = "ChoosingSten";
+      fileChooserState = "ChoosingSten";
       fileChooser.setFileFilter(pngFileFilter);
       fileChooser.showOpenDialog(frame);
     } else if (event.getSource() == saveButton) {
-      state = "ChoosingDest";
+      fileChooserState = "ChoosingDest";
       fileChooser.setFileFilter(pngFileFilter);
       fileChooser.showSaveDialog(frame);
     } else if (event.getSource() == selectMaskButton) {
-      state = "ChoosingMask";
+      fileChooserState = "ChoosingMask";
       fileChooser.setFileFilter(imagesFileFilter);
       fileChooser.showOpenDialog(frame);
     } else if (event.getSource() == selectSourceButton) {
-      state = "ChoosingSource";
+      fileChooserState = "ChoosingSource";
       fileChooser.setFileFilter(null);
       fileChooser.showOpenDialog(frame);
     } else if (event.getSource() == followUpFileChooser) {
@@ -207,7 +200,7 @@ public class StenographyDemo implements ActionListener {
     } else if (event.getSource() == fileChooser) {
       String actionCommand = event.getActionCommand();
       if (actionCommand.equals(JFileChooser.APPROVE_SELECTION)) {
-        if (state.equals("ChoosingSource")) {
+        if (fileChooserState.equals("ChoosingSource")) {
 
           File file = fileChooser.getSelectedFile();
 
@@ -219,7 +212,7 @@ public class StenographyDemo implements ActionListener {
           } catch (IOException e) {
             statusLabel.setText("Error opening source file.");
           }
-        } else if (state.equals("ChoosingMask")) {
+        } else if (fileChooserState.equals("ChoosingMask")) {
           File file = fileChooser.getSelectedFile();
           try {
             mask = ImageIO.read(file);
@@ -228,11 +221,11 @@ public class StenographyDemo implements ActionListener {
           } catch (IOException e) {
             statusLabel.setText("Error opening mask file.");
           }
-        } else if (state.equals("ChoosingSten")) {
+        } else if (fileChooserState.equals("ChoosingSten")) {
           stenFile = fileChooser.getSelectedFile();
           System.out.println(stenFile);
           followUpFileChooser.showSaveDialog(frame);
-        } else if (state.equals("ChoosingDest")) {
+        } else if (fileChooserState.equals("ChoosingDest")) {
           File outputFile = fileChooser.getSelectedFile();
 
           TripleDES cipher = new TripleDES(getPaddedPassword());
