fd44cc56ba32ca1824c70a0ff15997f9141c4232
Adam Barrowman <adambarrowman@yahoo.ca>
2015-06-23 22:16:20 -0700

    Changed the dynamic byte queue so it better handles thread interruption.

--- src/stream/DynamicByteQueue.java
+++ src/stream/DynamicByteQueue.java
@@ -8,19 +8,19 @@ import java.util.concurrent.atomic.AtomicLong;
 import core.CryptoUtils;
 
 public class DynamicByteQueue {
-  
+
   private static final int CHUNK_SIZE = 1024;
-  
+
   private ConcurrentLinkedQueue<byte[]> byteQueue;
   private volatile int lastChunkIndex;
   private int firstChunkIndex;
   private byte[] front;
-  
+
   private AtomicBoolean doneAdding;
   private AtomicLong chunkCount;
   private Object readLock;
   private Object writeLock;
-  
+
   public DynamicByteQueue() {
     byteQueue = new ConcurrentLinkedQueue<byte[]>();
     lastChunkIndex = 0;
@@ -31,13 +31,13 @@ public class DynamicByteQueue {
     chunkCount = new AtomicLong(0);
     front = new byte[CHUNK_SIZE];
   }
-  
+
   public void write(byte[] bytes) {
     write(bytes, 0, bytes.length);
   }
-  
+
   public void write(byte[] bytes, int start, int length) {
-    synchronized(writeLock) {
+    synchronized (writeLock) {
       if (doneAdding.get()) {
         return;
       }
@@ -59,9 +59,9 @@ public class DynamicByteQueue {
       }
     }
   }
-  
+
   public void doneWriting() {
-    synchronized(writeLock) {
+    synchronized (writeLock) {
       if (!doneAdding.get()) {
         chunkCount.incrementAndGet();
         byteQueue.add(front);
@@ -69,43 +69,42 @@ public class DynamicByteQueue {
       }
     }
   }
-  
+
   public int read(byte[] bytes) {
     return read(bytes, 0, bytes.length);
   }
 
   public int read(byte[] bytes, int start, int length) {
-    int dataRead = 0;
-    synchronized(readLock) {
-      try {
-        int destPos = start;
-        int remaining = length;
-        while (dataRead < length) {
-          if (chunkCount.longValue() >= 2) {
-            int bytesRead = innerRead(bytes, remaining, destPos, CHUNK_SIZE - firstChunkIndex, byteQueue.peek());
-            remaining -= bytesRead;
-            dataRead += bytesRead;
-            destPos += bytesRead;
-            if (firstChunkIndex == CHUNK_SIZE) {
-              firstChunkIndex = 0;
-              byteQueue.poll();
-              chunkCount.decrementAndGet();
-            }
-          } else if (doneAdding.get()) {
-            if (firstChunkIndex == lastChunkIndex) {
-              return dataRead == 0 ? -1 : dataRead;
-            }
-            return dataRead + innerRead(bytes, remaining, destPos, lastChunkIndex - firstChunkIndex, byteQueue.peek());
-          } else {
-            //wait for more data to be written
-            Thread.sleep(0);
+    synchronized (readLock) {
+      int dataRead = 0;
+      int destPos = start;
+      int remaining = length;
+      while (dataRead < length) {
+        if (chunkCount.longValue() >= 2) {
+          int bytesRead = innerRead(bytes, remaining, destPos, CHUNK_SIZE - firstChunkIndex, byteQueue.peek());
+          remaining -= bytesRead;
+          dataRead += bytesRead;
+          destPos += bytesRead;
+          if (firstChunkIndex == CHUNK_SIZE) {
+            firstChunkIndex = 0;
+            byteQueue.poll();
+            chunkCount.decrementAndGet();
+          }
+        } else if (doneAdding.get()) {
+          if (firstChunkIndex == lastChunkIndex) {
+            return dataRead == 0 ? -1 : dataRead;
+          }
+          return dataRead + innerRead(bytes, remaining, destPos, lastChunkIndex - firstChunkIndex, byteQueue.peek());
+        } else {
+          try {
+            Thread.sleep(0); // wait for more data to be written
+          } catch (InterruptedException e) {
+            e.printStackTrace();
           }
         }
-      } catch (InterruptedException e) {
-        e.printStackTrace();
       }
+      return dataRead;
     }
-    return dataRead;
   }
 
   private int innerRead(byte[] bytes, int length, int destPos, int bufferRemaining, byte[] buffer) {
