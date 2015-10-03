3749aec10589933a4eb14c2f5c5a70739d376f80
Adam Barrowman <adambarrowman@yahoo.ca>
2015-10-03 16:18:34 -0400

    Added a method for synchronously creating an input streams from the output of a stream runnable.

--- src/me/abarrow/stream/StreamRunnable.java
+++ src/me/abarrow/stream/StreamRunnable.java
@@ -49,7 +49,7 @@ public abstract class StreamRunnable implements Runnable {
   public synchronized final IOException getFailureReason() {
     return failure;
   }
-
+  
   public final InputStream startAsync(InputStream in) throws IOException {
     src = in;
     closeOnEnd = true;
@@ -59,7 +59,20 @@ public abstract class StreamRunnable implements Runnable {
     startOnNewThread();
     return related;
   }
-
+  
+  /**
+   * This can potentially use up a crazy high amount of RAM, since it buffers the entire output before returning,
+   * so be careful what input streams you use it with.
+   * @param in
+   * @return 
+   * @throws IOException
+   */
+  public final InputStream startSync(InputStream in) throws IOException {
+    DynamicByteQueue q = new DynamicByteQueue();
+    startSync(in, q.getOutputStream(), true);
+    return q.getInputStream();
+  }
+  
   private void startOnNewThread() {
     Thread thread = new Thread(this); //this should be replaced to use thread pools of some sort
     thread.start();
